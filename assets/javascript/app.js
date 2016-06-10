c64b13806ae6719da3c20082090f5d44999028c1
Orvin Welchez <owelchez@gmail.com>
2016-06-10 01:12:12 -0400

    Anything

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
+var capitals =
+{
+    country: "Honduras",
+    city: "Tegucigalpa",
+}
+
+	console.log(capitals);
+	console.log(capitals.country);
+	console.log(capitals.city);
 
 
 
 
 
-
-
-
-
-
-var capitales = {
-    country: "",
-    capital: "",
-    },
