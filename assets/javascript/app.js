3a33bf821e4e5b9f63120e56cdbc4e469724b403
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-06-10 20:16:00 -0400

    Layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,21 @@
-var flagArray = ['Argentina', 'Belgium', 'Bolivia', 'Brazil', 'Canada', 'Chile', 'China', 'Colombia',
+var countryArray = ['Argentina', 'Belgium', 'Bolivia', 'Brazil', 'Canada', 'Chile', 'China', 'Colombia',
                 'Cuba', 'Dominican Republic', 'Ecuador', 'El Salvador', 'France', 'Germany', 'Greece',
                 'Guatemala', 'Honduras', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Mexico', 'Netherlands',
                 'Nicaragua', 'Panama', 'Peru', 'Philippines', 'Poland', 'Portugal', 'South Africa', 'South Korea',
-                'Spain', 'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Uruguay', 'USA'];  
+                'Spain', 'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Uruguay', 'USA']; 
 
-	var randomFlag = flagArray[Math.floor(Math.random() * flagArray.length)];
-	console.log(randomFlag);
+var flagsArray = newArray("../images/flagArgentina.png", "../images/flagBelgium.png", "../images/flagBolivia.png");
 
- $('#question1').append('<span id="question1">' + randomFlag + '</span>');
+var randomNumber = Math.floor(Math.random() * newArray.length);
+
+$('.flagsGroup').append('<div class="randomFlag">' newArray[randomNumber]'</div>');
+
+// Fix this shit dude!!!
+
+	var randomCountry = countryArray[Math.floor(Math.random() * countryArray.length)];
+	console.log(randomCountry);
+
+ $('#question1').append('<span id="question1">' + randomCountry + '</span>');
 
 
 $('.panel-body').append('<div id="flags1">' + '</div>');
