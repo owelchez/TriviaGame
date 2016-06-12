efa796ce4ad9acf8dce900c4d347e405dc7aa6b0
Orvin Welchez <owelchez@gmail.com>
2016-06-11 20:17:22 -0400

    Layout

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ You are creating a basic trivia game with multiple choice or true/false options.
 The player will have a limited time length to finish the game. When the time runs out (regardless of whether or not they finished), the game will end and they will be shown the number of answers they got correct and incorrect.
 There will be a counter counting down the remaining time.
 The player should NOT be able to pick more than one answer for each question.
-Advanced Assignment User Story
 
+Advanced Assignment User Story
 Only one question will be shown at a time.
 The time limit will apply to each question, and when the time runs out, the game will show a screen with the correct answer, and then move on to the next question without user input.
 If the user chooses the correct answer, the game will show a screen letting them know the answer was correct, and then move on to the next question without user input.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,33 @@
-var countryArray = ['Argentina', 'Belgium', 'Bolivia', 'Brazil', 'Canada', 'Chile', 'China', 'Colombia',
+/*var countryArray = ['Argentina', 'Belgium', 'Bolivia', 'Brazil', 'Canada', 'Chile', 'China', 'Colombia',
                 'Cuba', 'Dominican Republic', 'Ecuador', 'El Salvador', 'France', 'Germany', 'Greece',
                 'Guatemala', 'Honduras', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Mexico', 'Netherlands',
                 'Nicaragua', 'Panama', 'Peru', 'Philippines', 'Poland', 'Portugal', 'South Africa', 'South Korea',
                 'Spain', 'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Uruguay', 'USA']; 
 
-var flagsArray = newArray("../images/flagArgentina.png", "../images/flagBelgium.png", "../images/flagBolivia.png");
-
-var randomNumber = Math.floor(Math.random() * newArray.length);
-
-$('.flagsGroup').append('<div class="randomFlag">' newArray[randomNumber]'</div>');
-
-// Fix this shit dude!!!
-
-	var randomCountry = countryArray[Math.floor(Math.random() * countryArray.length)];
-	console.log(randomCountry);
-
- $('#question1').append('<span id="question1">' + randomCountry + '</span>');
+var flagsArray = ["../images/flagArgentina.png", "../images/flagBelgium.png", "../images/flagBolivia.png",
+				"../images/flagBrazil.png", "../images/flagCanada"];*/
 
 
-$('.panel-body').append('<div id="flags1">' + '</div>');
+ 
 
 
+
+
+
+
+
+
+
+
+// $('#class1').append('<span>' + '<p>'Which of the following group of flags belong to '</p>''</span>');
+
+// $('.flagsGroup').append(('<div class="flagImages">') + flagsArray[0]);
+
+// var randomNumber = Math.floor(Math.random() * flagsArray.length);
+
+
+// $('.panel-body').append('<div id="flags1">' + countryArray[1] + '</div>');
+
+/* var randomCountry = countryArray[Math.floor(Math.random() * countryArray.length)];
+	console.log(randomCountry);*/
 
