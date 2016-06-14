eb20fa93d7ac0200e0cbfdd5428771decf2f753c
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-06-14 16:19:45 -0400

    Nothing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,8 +45,6 @@ var stopwatch =
     }
 };
 
-function initializeGame()
-{
 	clock();
 
 	correctFlags = 0;
@@ -57,36 +55,102 @@ $('#question1, #question2, #question3, #question4, #question5, #question6, #ques
 $('#start').on('click', function(){
         	$('#start').hide();
         	$('h3').hide();
-        	$('#question1').show();
+            $('#question1, #question2, #question3, #question4, #question5, #question6, #question7, #question8, #question9, #question10').show();
+
+        	//$('#question1').show();
         })
 
-};
 
-function question1()
-{
-	$('.rightAnswer').one('click', function()
-	{
-		correctFlags++;
-		$('.wrongAnswer').hide();
+	$('.rightAnswer1').on('click', function(){
+        correctFlags++;
+        $('.wrongAnswer1').one('click', function(){
+            $('.wrongAnswer1').hide();
+            wrongFlags++;
+            $('.rightAnswer1').off('click');          
+        })
 	})
 
-		$('.wrongAnswer').one('click', function()
-		{
-			wrongFlags++;
-			$('.wrongAnswer').hide();
-			$('.rightAnswer').off('click');
-		})
+    $('.rightAnswer2').one('click', function(){
+        correctFlags++;
+    })
 
-};
+    $('.rightAnswer3').one('click', function(){
+        correctFlags++;
+    })
 
+    $('.rightAnswer4').one('click', function(){
+        correctFlags++;
+    })
 
+    $('.rightAnswer5').one('click', function(){
+        correctFlags++;
+    })
 
+    $('.rightAnswer6').one('click', function(){
+        correctFlags++;
+    })
 
+    $('.rightAnswer7').one('click', function(){
+        correctFlags++;
+    })
 
-initializeGame();
-question1();
+    $('.rightAnswer8').one('click', function(){
+        correctFlags++;
+    })
+
+    $('.rightAnswer9').one('click', function(){
+        correctFlags++;
+    })
+
+    $('.rightAnswer10').one('click', function(){
+        correctFlags++;
+    })
+
+		
+
+  
+
+    
 
 console.log(correctFlags);
+console.log(wrongFlags);
 
 
 
+
+
+
+
+
+
+
+
+  /*$('.rightAnswer2').one('click', function()
+    {
+        $('.wrongAnswer2').hide();
+        correctFlags++;
+        console.log(correctFlags + " corret flags");
+        console.log(wrongFlags + " wrong flags");
+        alert('You got that right!');
+    })*/
+    
+/*
+    The two key methods to use with JavaScript are:
+
+setTimeout(function, milliseconds)
+Executes a function, after waiting a specified number of milliseconds.
+setInterval(function, milliseconds)
+Same as setTimeout(), but repeats the execution of the function continuously.
+
+How to Stop the Execution?
+The clearTimeout() method stops the execution of the function specified in setTimeout().
+
+window.clearTimeout(timeoutVariable)
+The window.clearTimeout() method can be written without the window prefix.
+
+The clearTimeout() method uses the variable returned from setTimeout():
+
+myVar = setTimeout(function, milliseconds);
+clearTimeout(myVar);
+If the function has not already been executed, you can stop the execution by calling the clearTimeout() method:
+*/
