1c8ca2e8067f5449059e279b95b0e99d9535a511
Orvin Welchez <owelchez@gmail.com>
2016-11-21 23:24:09 -0500

    Loading

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,48 +3,74 @@ var wrongFlags = 0;
 var totalFlags = 20;
 var minutes = 0;
 var seconds = 0;
-var stopTime;
+var stopTime = 0;
+var $questions = 0;
+var activeFlag = '';
 
-function clock(){
-	$('#start').on('click', stopwatch.start);
-};
+var flags = [  "flagHonduras", "flagJapan", "flagJamaica",
+                "flagUSA", "flagPanama", "flagGermany",
+                "flagArgentina", "flagAustria", "flagBarbados",
+                "flagBelgium", "flagBelize", "flagBolivia",
+                "flagBrazil", "flagBulgaria", "flagCanada",
+                "flagChile", "flagChina", "flagColombia",
+                "flagCuba", "flagDominicanRepublic", "flagEcuador",
+                "flagEgypt", "flagElSalvador", "flagEstonia",
+                "flagFrance", "flagGreece", "flagGuatemala",
+                "flagIndia", "flagIreland", "flagIsrael",
+                "flagItaly", "flagJamaica", "flagKenya",
+                "flagLaos", "flagMadagascar", "flagMexico",
+                "flagMongolia", "flagNetherlands", "flagNicaragua",
+                "flagPanama", "flagPeru", "flagPhilippines",
+                "flagPoland", "flagPortugal", "flagSaudiArabia",
+                "flagSlovakia", "flagSouthAfrica", "flagSouthKorea",
+                "flagSpain", "flagSweden", "flagSwitzerland",
+                "flagThailand", "flagTurkey", "flagUruguay",
+                "flagUSA"
+            ];
 
-var stopwatch = {
-	time:0,
+function getRandomFlag(){
+    activeFlag = flags[Math.floor(Math.random() * (1 + flags.length - 1))];
+}
 
-    start: function(){
-        counter = setInterval(stopwatch.count, 1000);
-    },
+    function clock(){
+    	$('#start').on('click', stopwatch.start);
+    };
 
-    stop: function(){
-        clearInterval(counter);
-    },
-    
-    count: function(){
-        stopwatch.time++;
-        var converted = stopwatch.timeConverter(stopwatch.time);
-        $('#timer').html(converted);
-    },
+    var stopwatch = {
+    	time:0,
 
-    timeConverter: function(t){
-        minutes = Math.floor(t/60);
-        seconds = t - (minutes * 60);
-        if (seconds < 10){
-            seconds = "0" + seconds;
+        start: function(){
+            counter = setInterval(stopwatch.count, 1000);
+        },
+
+        stop: function(){
+            clearInterval(counter);
+        },
+        
+        count: function(){
+            stopwatch.time++;
+            var converted = stopwatch.timeConverter(stopwatch.time);
+            $('#timer').html(converted);
+        },
+
+        timeConverter: function(t){
+            minutes = Math.floor(t/60);
+            seconds = t - (minutes * 60);
+            if (seconds < 10){
+                seconds = "0" + seconds;
+            }
+            if (minutes === 0){
+                minutes = "00";
+            } else if (minutes < 10){
+                minutes = "0" + minutes;
+            }
+            return minutes + ":" + seconds;
         }
-        if (minutes === 0){
-            minutes = "00";
-        } else if (minutes < 10){
-            minutes = "0" + minutes;
-        }
-        return minutes + ":" + seconds;
-    }
-};
+    };
+
+ 
 
 	clock();
-
-	correctFlags = 0;
-	wrongFlags = 0;
 
 $('#questions').hide();
 
@@ -54,7 +80,7 @@ $('#start').on('click', function(){
             $('#questions').show();
         })
 
-$('#')
+
 
 
 
